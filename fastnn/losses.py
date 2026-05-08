9490acd2ec3e27d45660080289fa6500e51af8d1
PetrouilFan <pfanioudakis@gmail.com>
2026-05-08 16:36:43 +0300

    Re-add fastnn/losses.py as re-export from _core (needed by __init__.py)

---
+++ fastnn/losses.py
@@ -0,0 +1,15 @@
+"""Loss functions exported by the Rust core."""
+
+import fastnn._core as _core
+
+mse_loss = _core.mse_loss
+cross_entropy_loss = _core.cross_entropy_loss
+bce_with_logits = _core.bce_with_logits
+huber_loss = _core.huber_loss
+
+__all__ = [
+    "mse_loss",
+    "cross_entropy_loss",
+    "bce_with_logits",
+    "huber_loss",
+]
